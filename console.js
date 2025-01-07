928f1d03d4307332cb0f11655d2a414b40b3961f
swnevin <swnevin@kihjelp.no>
2025-01-07 15:41:23 +0100

    Update console.js

--- console.js
+++ console.js
@@ -121,3 +121,30 @@ const FormExtension = {
     element.appendChild(formContainer);
   },
 };
+
+
+let proactiveMessage = "Hei, jeg hjelper deg gjerne!👋";
+
+    (function(d, t) {
+      var v = d.createElement(t), s = d.getElementsByTagName(t)[0];
+      v.onload = function() {
+        window.voiceflow.chat.load({
+          verify: { projectID: '677a8046ae2ed1739d0cc9c6' },
+          url: 'https://general-runtime.voiceflow.com',
+          versionID: 'production',
+          allowDangerousHTML: true,
+          assistant: {
+            extensions: [FormExtension]
+          }
+        }).then(() => {
+          console.log('Voiceflow widget loaded successfully');
+          window.voiceflow.chat.proactive.clear();
+          window.voiceflow.chat.proactive.push(
+            { type: 'text', payload: { message: proactiveMessage } }
+          );
+        }).catch((err) => console.error('Widget load failed:', err));
+      };
+      v.src = "https://cdn.voiceflow.com/widget/bundle.mjs";
+      v.type = "text/javascript";
+      s.parentNode.insertBefore(v, s);
+    })(document, 'script');
